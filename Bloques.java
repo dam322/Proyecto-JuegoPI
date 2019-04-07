be9cd896ae426074e6a508addc23d0d652f7fea3
Alifend <47732101+Alifend@users.noreply.github.com>
2019-04-06 19:25:38 -0500

    Editado por Celis

--- Bloques.java
+++ Bloques.java
@@ -10,35 +10,41 @@ public class Bloques extends Estructura {
     
     public Bloques(int x, int y, int ancho, int alto) {
         super(x, y, ancho, alto);
-        dureza=rand.nextInt(4) + 0;
+        dureza=rand.nextInt(5) + 0;
         System.out.println(dureza);
     }
 
-    public void draw(Graphics g){
+    public void draw(Graphics g) {
         switch (dureza) {
             case 0:
-                
                 break;
             case 1:
-                g.drawImage(LADRILLO2, x, y,ancho,alto,null);
+                g.drawImage(LADRILLO1, x, y, ancho, alto, null);
                 break;
             case 2:
-                g.drawImage(LADRILLO1, x, y,ancho,alto,null);
+                g.drawImage(LADRILLO2, x, y, ancho, alto, null);
                 break;
-            default:
-                g.drawImage(LADRILLO3, x, y,ancho,alto,null);
+            case 3:
+                g.drawImage(LADRILLO3, x, y, ancho, alto, null);
                 break;
+            case 4:
+                g.drawImage(BRONCE, x, y, ancho, alto, null);
+                break;
+
         }
-        
-       
-        
+
     }
     
 
 
     public void golpe(){
+        if (dureza != 4) {
+            dureza--;
+            Sonidos sound = new Sonidos("golpe");
+        
+        }
         Sonidos sound = new Sonidos("golpe");
-        dureza--;
+        
         
     }
     
@@ -46,6 +52,12 @@ public class Bloques extends Estructura {
 
     public int getDureza() {
         return dureza;
+    }
+    
+    
+    public void SetDureza(int dureza) {
+
+        this.dureza = dureza;
     }
     
     
